2d314c01a3935599983ecae99d8329a9e2c88854
highing666 <bigseabigdream@gmail.com>
2020-07-10 23:16:15 +0800

    tree depth

---
+++ src/leetcode/Coding_Interviews/tree_depth.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+class TreeNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def max_depth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        return max(self.max_depth(root.left), self.max_depth(root.right)) + 1
