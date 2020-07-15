905875666122c4f2793e4db0994c91c23b34a970
chenhaiyang <bigseabigdream@gmail.com>
2020-07-15 20:59:35 +0800

    two numbers with sum

---
+++ src/leetcode/Coding_Interviews/two_numbers_with_sum.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+
+    @staticmethod
+    def two_sum(nums: List[int], target: int) -> List[int]:
+        i, j = 0, len(nums) - 1
+        while i < j:
+            s = nums[i] + nums[j]
+            if s > target:
+                j -= 1
+            elif s < target:
+                i += 1
+            else:
+                return [nums[i], nums[j]]
+
+        return []
