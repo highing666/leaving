f0394906d7d1df6f94b13024617af34bb0a5be09
highing666 <bigseabigdream@gmail.com>
2021-05-08 21:40:47 +0800

    top k frequent elements

---
+++ src/leetcode/91lalg/top_k_frequent_elements.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+from heapq import heappush, heapreplace
+import heapq
+from typing import List
+
+
+class Solution:
+
+    def top_k_frequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        heap = []
+        for key, val in count.items():
+            if len(heap) >= k:
+                if val > heap[0][0]:
+                    heapreplace(heap, (val, key))
+            else:
+                heappush(heap, (val, key))
+
+        return [item[1] for item in heap]
