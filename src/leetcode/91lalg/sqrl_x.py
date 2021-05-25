edce31ea0eeb72751ccdd1cf768034c593a7e4a8
highing666 <bigseabigdream@gmail.com>
2021-05-25 21:21:09 +0800

    sqrt(x)

---
+++ src/leetcode/91lalg/sqrl_x.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+
+    def my_sqrt(self, x: int) -> int:
+        l, r, ans = 0, x, -1
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid <= x:
+                ans = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return ans
